7c4f0642e0515c10cc184cb0a93fc76b4c31d949
cpeadmin <cpeadmin@ubuntu2.myguest.virtualbox.org>
2024-05-26 05:42:15 +0800

    developed docker-compose file to up all node-app

---
+++ node-app/node-app-org1/index.js
@@ -0,0 +1,94 @@
+/*
+ * Copyright IBM Corp. All Rights Reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict';
+const express = require('express');
+const app = express();
+const port = 3001;
+
+const { Gateway, Wallets } = require('fabric-network');
+const path = require('path');
+const { buildCCPOrg1, buildWallet } = require('./AppUtil.js');
+// const channelName = process.env.CHANNEL_NAME || 'mychannel';
+// const chaincodeName = process.env.CHAINCODE_NAME || 'basic';
+// const SensorDataContract = require('../fabric_codes/cc-test/chaincode-javascript/lib/sensorDataContract.js');
+const channelName = 'c1';
+const chaincodeName = 'sensorDataContract1';
+const mspOrg1 = 'Org1MSP';
+const walletPath = path.join(__dirname, 'wallet');
+const org1UserId = 'node-app-user1';
+
+function prettyJSONString(inputString) {
+	return JSON.stringify(JSON.parse(inputString), null, 2);
+}
+
+async function main() {
+	try {
+		// build an in memory object with the network configuration (also known as a connection profile)
+		const ccp = buildCCPOrg1();
+
+		// build an instance of the fabric ca services client based on
+		// the information in the network configuration
+		// const caClient = buildCAClient(FabricCAServices, ccp, 'ca.org1.example.com');
+
+		// setup the wallet to hold the credentials of the application user
+		const wallet = await buildWallet(Wallets, walletPath, org1UserId);
+
+		// Create a new gateway instance for interacting with the fabric network.
+		// In a real application this would be done as the backend server session is setup for
+		// a user that has been verified.
+		const gateway = new Gateway();
+
+		try {
+			// setup the gateway instance
+			// The user will now be able to create connections to the fabric network and be able to
+			// submit transactions and query. All transactions submitted by this gateway will be
+			// signed by this user using the credentials stored in the wallet.
+			await gateway.connect(ccp, {
+				wallet,
+				identity: org1UserId,
+				discovery: { enabled: true, asLocalhost: false } // using asLocalhost as this gateway is using a fabric network deployed locally
+			});
+
+			// Build a network instance based on the channel where the smart contract is deployed
+			const network = await gateway.getNetwork(channelName);
+
+			// Get the contract from the network.
+			const contract = network.getContract(chaincodeName);
+
+			// Let's try a query type operation (function).
+			// This will be sent to just one peer and the results will be shown.
+			console.log('\n--> Evaluate Transaction: GetLatestSensorReadings, function returns all the current sensor readings based on sensorID on the ledger');
+			let result = await contract.evaluateTransaction('GetLatestSensorReadings');
+			console.log(`*** Result: ${prettyJSONString(result.toString())}`);
+
+            const data = JSON.parse(result.toString())
+            
+            // Disconnect from the gateway when the application is closing
+            await gateway.disconnect();
+
+			return data;
+        } catch (error) {
+            console.error(`******** FAILED to query data: ${error}`);
+            process.exit(1);
+        }
+	} catch (error) {
+		console.error(`******** FAILED to run the application: ${error}`);
+		process.exit(1);
+	}
+}
+
+app.get('/data', async (req, res) => {
+	  const data = await main();
+	res.json(data);
+	});
+	
+app.listen(port, () => {
+	console.log(`Middleware service listening at http://node-app-service1:${port}`);
+	console.log(`Data query can now be accessed at http://node-app-service1:${port}/data`);
+	// console.log(`Middleware service listening at http://localhost:${port}`);
+	// console.log(`Data query can now be accessed at http://localhost:${port}/data`);
+});
---
+++ node-app/node-app-org2/AppUtil.js
@@ -0,0 +1,96 @@
+/*
+ * Copyright IBM Corp. All Rights Reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
+exports.buildCCPOrg2 = () => {
+	// load the common connection configuration file
+	// const ccpPath = path.resolve(__dirname, '..', 'fabric_codes', 'test-net', 'organizations', 'peerOrganizations', 'org1.example.com', 'connection-org1.json');
+	const ccpPath = path.resolve(__dirname,'/tmp', 'connection-app-org2.json');
+	const fileExists = fs.existsSync(ccpPath);
+	if (!fileExists) {
+		throw new Error(`no such file or directory: ${ccpPath}`);
+	}
+	const contents = fs.readFileSync(ccpPath, 'utf8');
+
+	// build a JSON object from the file contents
+	const ccp = JSON.parse(contents);
+
+	console.log(`Loaded the network configuration located at ${ccpPath}`);
+	return ccp;
+};
+
+exports.buildWallet = async (Wallets, walletPath, userName) => {
+	// Create a new  wallet : Note that wallet is for managing identities.
+	let wallet;
+	if (walletPath) {
+		wallet = await Wallets.newFileSystemWallet(walletPath);
+		console.log(`Built a file system wallet at ${walletPath}`);
+	} else {
+		wallet = await Wallets.newInMemoryWallet();
+		console.log('Built an in memory wallet');
+	}
+
+    // Check to see if we've already enrolled the user.
+    const identity = await wallet.get(userName);
+    if (!identity) {
+      console.log('An identity for the user', userName, 'does not exist in the wallet');
+    //   return wallet;
+    }
+	try {
+		// // Create a new file system based wallet for managing identities.
+		const walletPath = path.join(process.cwd(), 'wallet');
+		// const wallet = await Wallets.newFileSystemWallet(walletPath);
+		// console.log(`Wallet path: ${walletPath}`);
+	
+		// Check to see if we've already imported the user.
+		const userExists = await wallet.get(userName);
+		if (userExists) {
+		  console.log('An identity for the user', userName, 'already exists in the wallet');
+		  return wallet;
+		}
+	
+		// Path to the user's certificate and private key
+		const certPath = path.resolve(__dirname, './organizations/peerOrganizations/org2.example.com/users/User1@org2.example.com/msp/signcerts/User1@org2.example.com-cert.pem');
+		const keyPath = path.resolve(__dirname, './organizations/peerOrganizations/org2.example.com/users/User1@org2.example.com/msp/keystore/priv_sk');
+	
+		// Read the certificate and private key files
+		const cert = fs.readFileSync(certPath).toString();
+		const key = fs.readFileSync(keyPath).toString();
+	
+		// Create the identity object
+		const x509Identity = {
+		  credentials: {
+			certificate: cert,
+			privateKey: key,
+		  },
+		  mspId: 'Org2MSP',
+		  type: 'X.509',
+		};
+	
+		// Import the identity into the wallet
+		await wallet.put(userName, x509Identity);
+		console.log('Successfully imported', userName, 'into the wallet');
+	
+	  } catch (error) {
+		console.error(`Failed to import identity: ${error}`);
+		process.exit(1);
+	  }
+	return wallet;
+};
+
+exports.prettyJSONString = (inputString) => {
+	if (inputString) {
+		 return JSON.stringify(JSON.parse(inputString), null, 2);
+	}
+	else {
+		 return inputString;
+	}
+}
+
