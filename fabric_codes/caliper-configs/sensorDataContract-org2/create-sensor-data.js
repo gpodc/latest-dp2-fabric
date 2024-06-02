9261cf9cab99df4bf3d45896863734c1eb0cc852
cpeadmin <cpeadmin@ubuntu2.myguest.virtualbox.org>
2024-06-02 18:58:40 +0800

    updated caliper-configs

--- fabric_codes/caliper-configs/sensorDataContract-org2/create-sensor-data.js
+++ fabric_codes/caliper-configs/sensorDataContract-org2/create-sensor-data.js
@@ -2,45 +2,45 @@
 
 const { WorkloadModuleBase } = require('@hyperledger/caliper-core');
 
-class ReadSensorDataWorkload extends WorkloadModuleBase {
+class CreateSensorDataWorkload extends WorkloadModuleBase {
     constructor() {
         super();
         this.txIndex = 6000;
-        this.limitIndex = 0;
     }
 
     async initializeWorkloadModule(workerIndex, totalWorkers, roundIndex, roundArguments, sutAdapter, sutContext) {
         await super.initializeWorkloadModule(workerIndex, totalWorkers, roundIndex, roundArguments, sutAdapter, sutContext);
-
-        this.limitIndex = this.roundArguments.assets;
-
     }
 
     async submitTransaction() {
+        // Unique sensor ID for each transaction
         this.txIndex++;
         let ID = 'data_' + this.txIndex.toString() + '_' + this.workerIndex;
+        let sensorID = '104';
+        let kwhReading = (Math.random() * 100).toFixed(2); // Generate a kwh meter reading
+        let ampReading = (Math.random() * 100).toFixed(2); // Generate a amp meter reading
+        let timestamp = new Date().toISOString();
 
         let args = {
-            contractId: 'sensorDataContract',
-            contractFunction: 'ReadSensorData',
+            contractId: 'sensorDataContract2',
+            contractFunction: 'CreateSensorData',
             contractVersion: 'v1',
             // invokerIdentity: 'User1',
-            contractArguments: [ID],
-            readOnly: true,
+            contractArguments: [ID, sensorID, kwhReading, ampReading,timestamp],
             timeout: 30
         };
 
         await this.sutAdapter.sendRequests(args);
     }
+    
 }
 
 /**
  * Create a new instance of the workload module.
  * @return {WorkloadModuleInterface}
  */
-
 function createWorkloadModule() {
-    return new ReadSensorDataWorkload();
+    return new CreateSensorDataWorkload();
 }
 
 module.exports.createWorkloadModule = createWorkloadModule;
---
+++ fabric_codes/caliper-configs/sensorDataContract-org3/create-sensor-data.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const { WorkloadModuleBase } = require('@hyperledger/caliper-core');
+
+class CreateSensorDataWorkload extends WorkloadModuleBase {
+    constructor() {
+        super();
+        this.txIndex = 6000;
+    }
+
+    async initializeWorkloadModule(workerIndex, totalWorkers, roundIndex, roundArguments, sutAdapter, sutContext) {
+        await super.initializeWorkloadModule(workerIndex, totalWorkers, roundIndex, roundArguments, sutAdapter, sutContext);
+    }
+
+    async submitTransaction() {
+        // Unique sensor ID for each transaction
+        this.txIndex++;
+        let ID = 'data_' + this.txIndex.toString() + '_' + this.workerIndex;
+        let sensorID = '104';
+        let kwhReading = (Math.random() * 100).toFixed(2); // Generate a kwh meter reading
+        let ampReading = (Math.random() * 100).toFixed(2); // Generate a amp meter reading
+        let timestamp = new Date().toISOString();
+
+        let args = {
+            contractId: 'sensorDataContract3',
+            contractFunction: 'CreateSensorData',
+            contractVersion: 'v1',
+            // invokerIdentity: 'User1',
+            contractArguments: [ID, sensorID, kwhReading, ampReading,timestamp],
+            timeout: 30
+        };
+
+        await this.sutAdapter.sendRequests(args);
+    }
+    
+}
+
+/**
+ * Create a new instance of the workload module.
+ * @return {WorkloadModuleInterface}
+ */
+function createWorkloadModule() {
+    return new CreateSensorDataWorkload();
+}
+
+module.exports.createWorkloadModule = createWorkloadModule;
